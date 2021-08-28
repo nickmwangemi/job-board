d71c38bf0b3fd4f8e5c8bb9db4d1bbdf9975522e
nickmwangemi <nickmwangemi@gmail.com>
2021-08-28 13:42:50 +0300

    Implement user schema for validation and password hashing

---
+++ backend/db/base.py
@@ -0,0 +1,3 @@
+from db.base_class import Base
+from db.models.job import Job
+from db.models.user import User
--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 
 from core.config import settings
-from db.base_class import Base
+from db.base import Base
 from db.session import engine
 
 

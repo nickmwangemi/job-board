602164e0102aba8ed9a67f08c459d4cbca497a80
nickmwangemi <nickmwangemi@gmail.com>
2021-08-28 15:35:06 +0300

    Hide implementation details of creted user

--- backend/apis/v1/routes/users.py
+++ backend/apis/v1/routes/users.py
@@ -3,12 +3,12 @@ from sqlalchemy.orm import Session
 
 from db.repository.user import create_new_user
 from db.session import get_db
-from schemas.user import UserCreate
+from schemas.user import UserCreate, ShowUser
 
 router = APIRouter()
 
 
-@router.post("")
+@router.post("", response_model=ShowUser)
 def create_user(user: UserCreate, db: Session = Depends(get_db)):
     user = create_new_user(user, db)
     return user

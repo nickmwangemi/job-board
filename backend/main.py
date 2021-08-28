bdf8eca8f3d765b12dc8b7097572445aaa110a1b
nickmwangemi <nickmwangemi@gmail.com>
2021-08-28 12:26:05 +0300

    Refactor config to add db configs

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
 
 from core.config import settings
+from db.base_class import Base
+from db.session import engine
 
-app = FastAPI(title=settings.PROJECT_TITLE, version=settings.PROJECT_VERSION)
+
+def create_tables():
+    Base.metadata.create_all(bind=engine)
+
+
+def start_application():
+    app = FastAPI(title=settings.PROJECT_TITLE, version=settings.PROJECT_VERSION)
+    create_tables()
+    return app
+
+
+app = start_application()
 
 
 @app.get("/")
